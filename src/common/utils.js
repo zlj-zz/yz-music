525a0cc88533c4432a8f0480ba81564f4f5500ef
Zachary <zlj19971222@outlook.com>
2021-02-13 23:57:17 +0800

    feat: add util method

---
+++ src/common/utils.js
@@ -0,0 +1,8 @@
+export function processCount(count) {
+  if (count > 10000000) {
+    return (count / 10000000).toFixed(2) + "亿";
+  } else if (count > 1000) {
+    return (count / 1000).toFixed(2) + "万";
+  }
+  return count;
+}
