bba645751920c9d2c9edd92d6da8c7e5525937fc
Zachary <zlj19971222@outlook.com>
2021-02-28 20:11:24 +0800

    feat: add axios retry

--- src/api/base.js
+++ src/api/base.js
@@ -1,10 +1,55 @@
 import axios from "axios";
+import { ElMessage } from "element-plus";
 
 const BASIC_URL = "http://zacharyzlj.cn:3000/";
 //const BASIC_URL = "http://192.168.31.152:3000/";
 
 function createBaseInstance() {
-  const instance = axios.create({ baseURL: BASIC_URL, withCredentials: true });
+  const instance = axios.create({
+    baseURL: BASIC_URL,
+    withCredentials: true,
+    retry: 3,
+    retryInterval: 2000
+  });
+  instance.retryDelay = 2000;
+  instance.defaults.timeout = 2000;
+  instance.interceptors.request.use(
+    function(config) {
+      return config;
+    },
+    function(error) {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    res => {
+      return res;
+    },
+    function(error) {
+      let config = error.config;
+
+      if (!config || !config.retry) return Promise.reject(error);
+
+      config.__retryCount = config.__retryCount || 0;
+
+      if (config.__retryCount >= config.retry) {
+        ElMessage.error("请检查你的网络，并尝试手动刷新");
+        return Promise.reject(error);
+      }
+
+      config.__retryCount++;
+
+      let backoff = new Promise(function(resolve) {
+        console.log("retry time: " + config.__retryCount);
+        resolve();
+      }, config.retryDelay || 1);
+
+      return backoff.then(function() {
+        return instance(config);
+      });
+    }
+  );
 
   return instance;
 }
--- src/components/common/LoginOpt.vue
+++ src/components/common/LoginOpt.vue
@@ -326,7 +326,6 @@ export default {
   margin-bottom: 6px;
   overflow: hidden;
   text-overflow: ellipsis;
-  display: box;
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
